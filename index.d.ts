dff98dcbc8460c1c4d221041ad2e860fea62d9d2
Ivan D. Gromov <lightrow@gmail.com>
2022-02-12 15:56:41 +0400

    add types and bump rn peer dep version

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const PortalOrigin: React.FunctionComponent<{ destination: string }>;
+
+export const PortalDestination: React.FunctionComponent<{
+  name: string;
+}>;
