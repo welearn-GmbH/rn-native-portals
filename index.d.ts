a9dff50f7f47756dc96cb724c7af0388a16657a8
Ivan D. Gromov <lightrow@gmail.com>
2022-08-03 12:31:38 +0200

    add ViewProps types to origin and destination

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react';
+import { ViewProps } from 'react-native';
 
-export const PortalOrigin: React.FunctionComponent<{
-  destination: string | null;
-}>;
+export const PortalOrigin: React.FunctionComponent<
+    {
+        destination: string | null;
+    } & ViewProps
+>;
 
-export const PortalDestination: React.FunctionComponent<{
-  name: string;
-}>;
+export const PortalDestination: React.FunctionComponent<
+    {
+        name: string;
+    } & ViewProps
+>;
